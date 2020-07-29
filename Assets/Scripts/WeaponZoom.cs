d7fb143c0774e55105f1214eb5511fd990d825ed
Zh00c3q <maciej.a.zak@gmail.com>
2020-07-29 12:49:26 +0200

    change mouse sinsitivity when zoomed in

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] float zoomIn = 30f;
     [SerializeField] float zoomOut = 60f;
+    [SerializeField] float zoomInSensitivity = 0.5f;
+    [SerializeField] float zoomOutSensitivity = 2f;
     bool zoomedInToggle = false;
+
+
+    RigidbodyFirstPersonController fpsController;
 
     void Start()
     {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
     }
      
     void Update()
@@ -20,11 +27,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 Camera.main.fieldOfView = zoomIn;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
             }
             else
             {
                 zoomedInToggle = false;
                 Camera.main.fieldOfView = zoomOut;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
         }
     }
