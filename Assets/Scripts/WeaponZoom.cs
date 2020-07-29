40850056984322c8c977f78d2c07c3de72e62b23
Zh00c3q <maciej.a.zak@gmail.com>
2020-07-29 12:39:47 +0200

    add weapon zoom in

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] float zoomIn = 30f;
+    [SerializeField] float zoomOut = 60f;
+    bool zoomedInToggle = false;
+
+    void Start()
+    {
+    }
+     
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                Camera.main.fieldOfView = zoomIn;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                Camera.main.fieldOfView = zoomOut;
+            }
+        }
+    }
+}
